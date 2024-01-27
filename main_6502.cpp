d11053c87d225345eb02cd02b8d3fe8b9aecb295
msagr <mehulsagrawal33@gmail.com>
2024-01-27 15:50:48 +0530

    ins. execution

--- main_6502.cpp
+++ main_6502.cpp
@@ -8,6 +8,20 @@ using u32 = unsigned int;
 struct Mem {
     static constexpr u32 MAX_MEM = 1024 * 64;
     Byte Data[MAX_MEM];
+
+    // initializing memory
+    void Initialise(){
+        for(u32 i=0; i<MAX_MEM; i++){
+            Data[i] = 0;
+        }
+    }
+
+    // read 1 byte
+    Byte operator[] (u32 Address) const {
+
+        // assert here Address is < MAX_MEM
+        return Data[Address];
+    }
 };
 
 struct CPU {
@@ -27,19 +41,41 @@ struct CPU {
     Byte N : 1;
 
     // reset cpu
-    void Reset(){
+    void Reset(Mem& memory){
         PC = 0xFFFC;
         SP = 0x0100;
-        D = 0;
 
         C = Z = I = D = B = V = N = 0;
         A = X = Y = 0;
+
+        // initialize memory
+        memory.Initialise();
+    }
+
+    Byte FetchByte(u32& cycle, Mem& memory){
+
+        Byte Data = memory[PC];
+        PC++;
+        cycle--;
+        return Data;
+    }
+
+    // to execute ins.
+    void Execute(u32 cycle,  Mem& memory){
+
+        while(cycle > 0){
+            Byte Ins = FetchByte(cycle, memory);
+        }
     }
 };
 
 int main(){
 
+    Mem mem;
     CPU cpu;
-    cpu.Reset();
+    cpu.Reset(mem);
+
+    // execute instruction
+    cpu.Execute(2, mem); // Execute(clock_cycle, memory)
     return 0;
 }
