b99b5738bccb2ee7956d09bdd4d3a297ea675b5d
msagr <mehulsagrawal33@gmail.com>
2024-01-29 17:42:06 +0530

    writing to memory

--- main_6502.cpp
+++ main_6502.cpp
@@ -22,6 +22,11 @@ struct Mem {
         // assert here Address is < MAX_MEM
         return Data[Address];
     }
+
+    // write 1 byte
+    Byte& operator[] (u32 Address) { // writing Byte& operator[] (u32 Address) const is giving error, why?
+        return Data[Address];
+    }
 };
 
 struct CPU {
@@ -60,11 +65,31 @@ struct CPU {
         return Data;
     }
 
+    // opcode
+    static constexpr Byte INS_LDA_IM = 0xA9;
+
     // to execute ins.
     void Execute(u32 cycle,  Mem& memory){
 
         while(cycle > 0){
             Byte Ins = FetchByte(cycle, memory);
+
+            switch(Ins){
+                case INS_LDA_IM:
+                {
+                    // fetching value which is 2nd byte
+                    Byte value = FetchByte(cycle, memory);
+                    
+                    A = value;
+                    Z = (A == 0);
+                    N = (A & 0b10000000) > 0;
+
+                } break;
+                default:
+                {
+                    printf("Instruction not handled %d", Ins);
+                } break;
+            }
         }
     }
 };
@@ -74,6 +99,11 @@ int main(){
     Mem mem;
     CPU cpu;
     cpu.Reset(mem);
+
+    // start - inline a little program
+    mem[0xFFFC] = CPU::INS_LDA_IM; // putting stuff in memory
+    mem[0xFFFD] = 0x42; 
+
 
     // execute instruction
     cpu.Execute(2, mem); // Execute(clock_cycle, memory)
